7271ffb72891720ed28b97cf1c4addfe250f3130
chloejordaan36-sys <chloejordaan36@gmail.com>
2026-06-01 00:57:36 +0200

    fix railway server crash

--- server/server1.js
+++ server/server1.js
@@ -9,7 +9,7 @@ const app = express();
    CONFIG
 ========================= */
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 /* =========================
    MIDDLEWARE
